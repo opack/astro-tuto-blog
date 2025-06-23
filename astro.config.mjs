6c33c084268aad2ce66c5007dc7354e3c755fe71
Opack <github@valdera.fr>
2025-06-23 12:06:23 +0200

    Add Preact `Greeting` component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-tuto-blog-valdera.netlify.app'
+  site: 'https://astro-tuto-blog-valdera.netlify.app',
+  integrations: [preact()]
 });
